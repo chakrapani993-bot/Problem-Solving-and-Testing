00ad37e22b9a105ce38b4b1fd0120b7e5b502bbd
Chakrpani <chakrapani993@gmail.com>
2026-08-10 09:45:16 +0530

    Add Java program to filter even numbers from a list
    This program filters and prints even numbers from a list.

---
+++ week3/Session13-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10, 15, 20, 25, 30);
+
+        list.stream()
+            .filter(n -> n % 2 == 0)
+            .forEach(System.out::println);
+    }
+}
+
+Output
+10
+20
+30
