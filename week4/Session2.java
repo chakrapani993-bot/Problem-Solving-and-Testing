4e75e9f579fc61bec1fe140d8ee4fbc7150dc48d
Chakrpani <chakrapani993@gmail.com>
2026-08-21 09:42:45 +0530

    Implement even number counter in Session2.java
    Added a program to count even numbers in an array.

---
+++ week4/Session2.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 11, 14, 7};
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
+Output
+3
