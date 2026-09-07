33152098161365e2f3b683970a52c977c7917167
Chakrpani <chakrapani993@gmail.com>
2026-09-07 09:31:26 +0530

    Create ParkingSystem class for managing parking slots
    Implement a ParkingSystem class with methods to add cars.

---
+++ week7/session1-1.java
@@ -0,0 +1,25 @@
+task 1 — Design Parking System
+  
+class ParkingSystem {
+
+    int[] slots;
+
+    public ParkingSystem(int big, int medium, int small) {
+        slots = new int[]{big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        if (slots[carType - 1] > 0) {
+            slots[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
+
+Input
+["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+[[1, 1, 0], [1], [2], [3], [1]]
+
+Output
+[null, true, true, false, false]
