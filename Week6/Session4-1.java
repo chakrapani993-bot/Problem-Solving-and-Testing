9e04124feaab905f7366894fdea40aceb34781e4
Chakrpani <chakrapani993@gmail.com>
2026-09-12 09:27:03 +0530

    Add PalindromeIndex class with palindromeIndex method
    Implement a method to find the index of a character that can be removed to make a string palindrome.

---
+++ Week6/Session4-1.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class PalindromeIndex {
+
+    public static int palindromeIndex(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+
+                if (isPalindrome(s, left + 1, right))
+                    return left;
+
+                if (isPalindrome(s, left, right - 1))
+                    return right;
+
+                return -1;
+            }
+
+            left++;
+            right--;
+        }
+
+        return -1;
+    }
+
+    public static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(palindromeIndex(s));
+    }
+}
+
+
+Input
+
+aaab
+
+Output
+
+3
